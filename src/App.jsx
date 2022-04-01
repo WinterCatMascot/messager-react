8dc2124f5a8b157b90b21f2e5286aeb5d5f56f45
WinterCatMascot <wintercatmascot@gmail.com>
2022-04-01 21:09:16 +0300

    add login btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 
 function App() {
+  const handleClick = () => {
+    alert('Войти не удалось!');
+  };
+
+  // function handleClick() {
+  //  alert('Войти не удалось!');
+  // }
+
   return (
     <div>
-      12345
+      <button onClick={handleClick}>
+        Login
+      </button>
     </div>
   )
 }
