6028ca3dbd54625b3bc53c91ecf126c6bf0686c2
DaryaLis <21dara02@gmail.com>
2022-04-01 22:03:44 +0300

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 
 function App() {
-  const handleClick = () => {
+    const [count, setCount] = useState(0) // state позволяет отображать изменения состояния (почти переменной) в браузере
+        // деструктуризация, мы взяли нулевой и первый элемент из массива, count хранит значение, setCount - функция, позв менять значение count
+    const handleClick = () => {
     alert('Войти не удалось!');
   };
+    function handlePlus() {
+        setCount(count+1)
+    }
+    function handleMinus() {
+        setCount(count-1)
+    }
 
   // function handleClick() {
   //  alert('Войти не удалось!');
@@ -11,9 +19,17 @@ function App() {
 
   return (
     <div>
+        <p>{count}</p>
+        {/*фигурные скобки - вставки js (переменная в данный момент = 0)*/}
       <button onClick={handleClick}>
-        Login
+        login
       </button>
+        <button onClick={handlePlus}>
+            +1
+        </button>
+        <button onClick={handleMinus}>
+            -1
+        </button>
     </div>
   )
 }
